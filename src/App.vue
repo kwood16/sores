75fff3fbcf45e425146f21dd8df37d9c5a05c127
Kevin Wood <kevinwood16@gmail.com>
2019-11-23 23:58:06 -0700

    Implement fhirclient and render application in EHR simulator using Launch Scenario. Displaying Patient resource for first two patients (Daniel X. Adams and Stan Oldman), so it should be able to work with any patient loaded as a persona and in a Launch Scenario. Also added a nav bar and bootstrap elements after install bootstrap and the fhirclient node modules.

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,18 @@
 <template>
-  <!-- <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div> -->
-  <router-view/>
+  <div id="app">
+    <b-navbar toggleable="md" type="dark" variant="dark">
+      <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
+      <b-navbar-brand to="/">Sores for Dayzzz</b-navbar-brand>
+      <b-collapse is-nav id="nav_collapse">
+        <b-navbar-nav>
+          <b-nav-item to="/">Home</b-nav-item>
+          <b-nav-item to="/intro">Introduction</b-nav-item>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+    <!-- routes will be rendered here -->
+    <router-view />
+  </div>
 </template>
 
 <script>
