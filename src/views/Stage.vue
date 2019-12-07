1c68fac2c73dc3d7bcaf87c33ba25d55643166ce
Kevin Wood <kevinwood16@gmail.com>
2019-12-07 13:33:52 -0700

    Fixed stages to display images dynamically by changing stages data to be 2-D array.

--- src/views/Stage.vue
+++ src/views/Stage.vue
@@ -3,7 +3,7 @@
         <br>
         <h1>Stage {{ $route.params.id }}</h1>
         <div id="stage-decision" class="card-body justify-content-center">
-            <b-card-img-group deck v-for="(pic, index) in stages" :key="index">
+            <b-card-img-group deck v-for="(pic, index) in stages[$route.params.id-1]" :key="index">
                 <b-card-img
                     :src="getPic(index)"
                     :alt="pic"
@@ -37,26 +37,34 @@ export default {
   data: function () {
     return {
       stages:[
-          'stage1-1.png',
-          'stage1-2.png',
-          'stage1-3.png',
-          'stage1-4.png',
-          'stage2-1.jpg',
-          'stage2-2.jpg',
-          'stage2-3.jpg',
-          'stage2-4.jpg',
-          'stage3-1.jpg',
-          'stage3-2.jpg',
-          'stage3-3.jpg',
-          'stage4-1.jpg',
-          'stage4-2.jpg',
-          'stage4-3.jpg'
+          [
+            'stage1-1.png',
+            'stage1-2.png',
+            'stage1-3.png',
+            'stage1-4.png'
+          ],
+          [
+            'stage2-1.jpg',
+            'stage2-2.jpg',
+            'stage2-3.jpg',
+            'stage2-4.jpg'
+          ],
+          [
+            'stage3-1.jpg',
+            'stage3-2.jpg',
+            'stage3-3.jpg',
+          ],
+          [
+            'stage4-1.jpg',
+            'stage4-2.jpg',
+            'stage4-3.jpg'
+          ]
       ]
     }
   },
   methods: {
     getPic(index) {
-        return require('../assets/' + this.stages[index]);
+        return require('../assets/' + this.stages[this.$route.params.id - 1][index]);
     }
   },
   computed: {
@@ -66,18 +74,6 @@ export default {
      })
    }
   }
-//   ,
-//    watch: {
-//     $route: function() {
-//         // Check if given route is true, if it is then hide Nav. 
-//         if (this.$route.path === "/stage/1") {
-//             store.commit('SHOWNAV');
-//             } else  {
-//             store.commit('HIDENAV');
-//         }
-//         }
-//     }
-
 
 }
 </script>
