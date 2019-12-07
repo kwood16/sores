796273d8da461e89814644228a8d143d8878b1bc
Kevin Wood <kevinwood16@gmail.com>
2019-12-07 13:04:04 -0700

    Pushed most recent changes to route to the app I created in the HSPC/Logica sandbox. Also tried commenting/uncommenting axios imports because they seem to create errors in the console. Also adjust the JS script in Home.vue to reflect Kyle's suggestions.

--- src/main.js
+++ src/main.js
@@ -1,13 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-// import axios from 'axios'
+import axios from 'axios'
 import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
+Vue.use(axios);
 Vue.use(BootstrapVue);
-// Vue.use(axios);
 Vue.config.productionTip = false
 
 new Vue({
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,7 +8,6 @@
         <img src="@/assets/caution.png">
         <div id="main">
             <br>
-            {{ pat_info }}
             <router-link to="/intro">
                 <b-button pill variant="primary" to="/intro">Continue to SORES for DAYZZZ</b-button>
             </router-link>
@@ -20,31 +19,33 @@
 import axios from 'axios';
 import * as FHIR from 'fhirclient'
 
-FHIR.oauth2.ready(function(smart){
-        var patient = smart.patient;
-        console.log(patient)
-//   .then(client => client.request("Patient"))
-//   .then(console.log(client))
-//   .catch(console.error);
-// FHIR.oauth2.ready(function(fhirClient){
-
-});
-
-FHIR.oauth2.ready().then(response => { console.log(response) })
-
 export default {
 data () {
     return {
       pat_info: null
     }
   },
+// created() {
+//     FHIR.oauth2.ready().then(response => { console.log(response) })
+//     axios.get('https://api.logicahealth.org/Testapalooza/open/Patient?_id=CUSTO-1005198-02')
+//         .then(response => //(this.pat_info = response)
+//             {
+//                 console.log('response', response.data.entry[0].resource);
+//             }
+//         );
+//     }
+
+// }
 created() {
-    axios.get('https://api.logicahealth.org/Testapalooza/open/Patient?_id=CUSTO-1005198-02')
-        .then(response => (this.pat_info = response)
-
-        );
+    FHIR.oauth2.ready()
+        .then(response => {
+            console.log(response);
+            axios.get('https://api.logicahealth.org/Testapalooza/open/Patient?_id=CUSTO-1005198-02')
+                .then(response => {
+                    console.log('response', response.data.entry[0].resource);
+                });
+        });
     }
-
 }
 
 </script>
